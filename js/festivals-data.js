7deb7ac1fd1eaaa9fd27f9e669f12620cf45faf4
Sérgio Ramada <111106259+ramadageografia@users.noreply.github.com>
2026-01-22 02:02:01 -0300

    Add festival data for Boom, Universo Paralello, and Ozora

--- js/festivals-data.js
+++ js/festivals-data.js
@@ -1,1 +1,24 @@
+const festivals = [
+  {
+    name: "Boom Festival",
+    country: "Portugal",
+    continent: "Europe",
+    lat: 39.716,
+    lng: -7.492
+  },
+  {
+    name: "Universo Paralello",
+    country: "Brasil",
+    continent: "South America",
+    lat: -12.541,
+    lng: -38.996
+  },
+  {
+    name: "Ozora Festival",
+    country: "Hungria",
+    continent: "Europe",
+    lat: 46.784,
+    lng: 18.095
+  }
+];
 
